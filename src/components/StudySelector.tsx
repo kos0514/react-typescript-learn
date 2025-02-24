c3a21bc97db105f711fcf6cc21b4af896f15bb3e
kos0514 <ghqyydf829@gmail.com>
2025-02-24 19:37:04 +0900

    StudySelectorを切り替え可能にする

--- src/components/StudySelector.tsx
+++ src/components/StudySelector.tsx
@@ -1,10 +1,35 @@
 import '@/App.css'
 import ToggleShowItem from "@/components/ToggleShowItem";
+import  {useState} from "react";
+import * as React from "react";
+
+const STUDY_COMPONENTS = {
+    ToggleShowItem: <ToggleShowItem />,
+} as const;
+
+type componentType = keyof typeof STUDY_COMPONENTS;
+
 
 const StudySelector = () => {
+    const [selectedComponent, setSelectedComponent] = useState<componentType>("ToggleShowItem");
+
+    const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setSelectedComponent(event.target.value as componentType);
+    }
+
+    const SelectedComponent = STUDY_COMPONENTS[selectedComponent];
+
     return (
         <div className="tree-box-style">
-            <ToggleShowItem />
+            <select value={selectedComponent} onChange={handleChange}>
+                {Object.keys(STUDY_COMPONENTS).map((component) => (
+                    <option key={component} value={component}>
+                        {component}
+                    </option>
+                ))}
+            </select>
+            <br /><br />
+            {SelectedComponent}
         </div>
     );
 };
